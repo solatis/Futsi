e8a8f76e3137522710ee4cd9705b763d3d2e1060
Leon Mergen <leon@solatis.com>
2015-03-02 09:52:30 +0700

    Initial commit
    Implements AST functions and test cases

---
+++ Futsi/Ast.fs
@@ -0,0 +1,23 @@
+﻿namespace Futsi
+
+module Ast = 
+    // A Token is a combination of a key and possibly an associated value
+    type Token =  string * string option
+
+    // A Line is just a list of tokens; the parser separates based on newlines.
+    type Line  = Token list
+
+    // Returns true if key exists within a list of tokens
+    let rec key (k1:string) (tokens:Token list) : bool = 
+        match tokens with
+        | []                        -> false
+        | (k2, _) :: _ when k1 = k2 -> true
+        | _       :: xs             -> key k1 xs
+
+    // Returns value if key was found, None otherwise
+    let rec value (k1:string) (tokens:Token list) : string option = 
+        match tokens with
+        | []                        -> None
+        | (k2, v) :: _ when k1 = k2 -> v
+        | _       :: xs             -> value k1 xs
+
---
+++ Futsu.Tests/AstTests.fs
@@ -0,0 +1,41 @@
+﻿module Futsi.Tests.Ast
+
+open Futsi.Ast
+open Fuchu
+open FsUnit
+
+[<Tests>]
+let tests =
+    testList "Ast Tests" [
+        testList "Looking up keys" [
+            testCase "Should return False on empty list" <| fun _ ->
+                key "foo" [] |> should equal false
+
+            testCase "Should return True when key without value exists" <| fun _ ->
+                key "foo" [("foo", None)] |> should equal true
+
+            testCase "Should return True when key with value exists" <| fun _ ->
+                key "foo" [("foo", Some("wombat"))] |> should equal true
+
+            testCase "Should return False when key without value does not exist" <| fun _ ->
+                key "bar" [("foo", None)] |> should equal false
+
+            testCase "Should return False when key with value exists" <| fun _ ->
+                key "bar" [("foo", Some("wombat"))] |> should equal false
+            ]
+
+        testList "Looking up values" [
+            testCase "Should return None on empty list" <| fun _ ->
+                value "foo" [] |> should equal None
+
+            testCase "Should return value when key/value exists" <| fun _ ->
+                value "foo" [("foo", Some("wombat"))] |> should equal (Some "wombat")
+
+            testCase "Should return None when only key exists" <| fun _ ->
+                value "foo" [("foo", None)] |> should equal None
+
+            testCase "Should return None when key does not exist" <| fun _ ->
+                value "bar" [("foo", Some("wombat"))] |> should equal None
+
+            ]
+        ]
