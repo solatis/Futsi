c93eb7e014b61a7308687a59f5eba9b3b2599492
Leon Mergen <leon@solatis.com>
2015-03-03 10:35:04 +0700

    Now refactoring back to Fuchu again

--- Futsi.Tests/Main.fs
+++ Futsi.Tests/Main.fs
@@ -1,15 +1,7 @@
 ﻿module Main
 
-open System
 open Fuchu
 
 // Added for Fuchu -- this allows the EXE to be its own test runner
 [<EntryPoint>]
-let main args = 
-    let exitCode = defaultMainThisAssembly args
-    
-    Console.WriteLine("Press any key")
-    Console.ReadLine() |> ignore
-
-    // return the exit code
-    exitCode 
+let main args = defaultMainThisAssembly args
