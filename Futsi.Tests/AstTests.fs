2eac475d2975124b5b8aa4bc652f88dac21118e5
Leon Mergen <leon@solatis.com>
2015-03-04 12:44:55 +0700

    Converts everything to nunit tests again

--- Futsi.Tests/AstTests.fs
+++ Futsi.Tests/AstTests.fs
@@ -3,38 +3,45 @@
 open Futsi.Ast
 open Fuchu
 open FsUnit
+open NUnit.Framework
 
-[<Tests>]
-let tests =
-    testList "Ast Tests" [
-        testList "Looking up keys" [
-            testCase "Should return False on empty list" <| fun _ ->
-                key "foo" [] |> should equal false
+[<TestFixture>]
+type Key() = 
 
-            testCase "Should return True when key without value exists" <| fun _ ->
-                key "foo" [("foo", None)] |> should equal true
+    [<Test>]
+    member this.``Should return False on empty list``() = 
+        key "foo" [] |> should equal false
 
-            testCase "Should return True when key with value exists" <| fun _ ->
-                key "foo" [("foo", Some("wombat"))] |> should equal true
+    [<Test>]
+    member this.``Should return True when key without value exists``() = 
+        key "foo" [("foo", None)] |> should equal true
 
-            testCase "Should return False when key without value does not exist" <| fun _ ->
-                key "bar" [("foo", None)] |> should equal false
+    [<Test>]
+    member this.``Should return True when key with value exists``() = 
+        key "foo" [("foo", Some("wombat"))] |> should equal true
 
-            testCase "Should return False when key with value exists" <| fun _ ->
-                key "bar" [("foo", Some("wombat"))] |> should equal false
-            ]
+    [<Test>]
+    member this.``Should return False when key without value does not exist``() = 
+        key "bar" [("foo", None)] |> should equal false
 
-        testList "Looking up values" [
-            testCase "Should return None on empty list" <| fun _ ->
-                value "foo" [] |> should equal None
+    member this.``Should return False when key with value exists``() = 
+        key "bar" [("foo", Some("wombat"))] |> should equal false
 
-            testCase "Should return value when key/value exists" <| fun _ ->
-                value "foo" [("foo", Some("wombat"))] |> should equal (Some "wombat")
+[<TestFixture>]
+type Value() = 
 
-            testCase "Should return None when only key exists" <| fun _ ->
-                value "foo" [("foo", None)] |> should equal None
+    [<Test>]
+    member this.``Should return None on empty list``() = 
+        value "foo" [] |> should equal None
 
-            testCase "Should return None when key does not exist" <| fun _ ->
-                value "bar" [("foo", Some("wombat"))] |> should equal None
-            ]
-        ]
+    [<Test>]
+    member this.``Should return value when key/value exists``() =
+        value "foo" [("foo", Some("wombat"))] |> should equal (Some "wombat")
+
+    [<Test>]
+    member this.``Should return None when only key exists``() = 
+        value "foo" [("foo", None)] |> should equal None
+
+    [<Test>]
+    member this.``Should return None when key does not exist``() = 
+        value "bar" [("foo", Some("wombat"))] |> should equal None
